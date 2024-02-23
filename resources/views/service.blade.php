97e5a847bf56c2e537b9c06b52bdf74543681c0a
Kenneth <kentvalencia21@gmail.com>
2024-02-23 17:02:58 +0800

    firstcommit

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
        
-        <title>welcome</title>
+        <title>service</title>
 
     </head>
    <body>
@@ -36,8 +36,8 @@
     Dropdown
   </button>
   <ul class="dropdown-menu">
-    <li><button class="dropdown-item1" type="button">Dropdown item</button></li>
-    <li><button class="dropdown-item2" type="button">Dropdown item</button></li>
-    <li><button class="dropdown-item3" type="button">Dropdown item</button></li>
+    <li><a class="dropdown-item1" type="button">Dropdown item</button></li> 
+    <li><a class="dropdown-item2" type="button">Dropdown item</button></li>
+    <li><a class="dropdown-item3" type="button">Dropdown item</button></li>
   </ul>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+    
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('service', function () {
+    return view('service');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('aboutus', function () {
+    return view('aboutus');
+});
+
+Route::get('logout', function () {
+    return view('logout');
+});
